194b351bd5cf9852d22017b2e7ed3b3203ff8273
Taras-Klimenko <tarasklimenko.1989@gmail.com>
2024-06-17 21:14:22 +0300

    Updated Category List component

---
+++ src/components/category-list/category-list.component.jsx
@@ -0,0 +1,14 @@
+import './category-list.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+
+
+const CategoryList = ({categories}) => {
+    return(<div className="categories-container">
+        {categories.map((category) => {
+          return <CategoryItem category={category} key={category.id} />;
+        })}
+      </div>)
+}
+
+export default CategoryList;
